77a29043b3715ccefbf568132f892d18bbcd93bd
TarunShetty256 <tarunshetty256@gmail.com>
2024-05-31 12:12:12 +0530

    first oush

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{width:"99%",height:"800px",borderRadius:"10px",background: "linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%)"}}>
+      <h1>About Page</h1>
+      
+
+      </div>
+
+  );
+}
+
+export default About;
